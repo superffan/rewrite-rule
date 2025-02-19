64e00193e6fb3529a2d3e22410498de56f8b3b40
superffan <33724896+superffan@users.noreply.github.com>
2025-02-19 18:03:57 +0800

    Update yudao.js

--- yudao.js
+++ yudao.js
@@ -1,13 +1,8 @@
 var body = $response.body;
-
-log('修改前->',body);
-
 var obj = JSON.parse(body);
 
 obj['code'] = 200;
 obj['succeeded'] = true;
 body = JSON.stringify(obj);
 
-log('修改后->',body);
-
-$done(body);
+$done({ body, status: 'HTTP/1.1 200 OK' });
