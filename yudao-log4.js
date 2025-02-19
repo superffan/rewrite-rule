e5a196b6ffcb35f6fed8f398fe547f9de75304be
superffan <33724896+superffan@users.noreply.github.com>
2025-02-19 17:43:06 +0800

    Update and rename yudao-log2.js to yudao-log4.js

--- yudao-log4.js
+++ yudao-log4.js
@@ -1,8 +1,10 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
+console.log('修改前->', { body })
 obj['code'] = 200;
 obj['succeeded'] = true;
 body = JSON.stringify(obj);
+console.log('修改后->', { body })
 
-$.done({ body });
+$done({ body });
