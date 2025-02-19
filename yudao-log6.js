1b5decdd874f2db3b210294d38ef58df1f7e2ed8
superffan <33724896+superffan@users.noreply.github.com>
2025-02-19 17:50:50 +0800

    Update and rename yudao-log5.js to yudao-log6.js

--- yudao-log6.js
+++ yudao-log6.js
@@ -1,9 +1,8 @@
 var body = $response.body;
-var headers = $response.headers;
 var obj = JSON.parse(body);
-console.log(headers);
+
 obj['code'] = 200;
 obj['succeeded'] = true;
 body = JSON.stringify(obj);
 
-$done({ body });
+$done({ body, status: 'HTTP/1.1 200 OK' });
