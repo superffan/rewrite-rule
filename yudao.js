3707153204c704518938cb46afb779dce2be1c71
superffan <33724896+superffan@users.noreply.github.com>
2025-02-19 17:23:13 +0800

    Update yudao.js

--- yudao.js
+++ yudao.js
@@ -1,10 +1,13 @@
 var body = $response.body;
+
+console.info('修改前->',body);
+
 var obj = JSON.parse(body);
 
 obj['code'] = 200;
 obj['succeeded'] = true;
 body = JSON.stringify(obj);
 
-console.error(body);
+console.info('修改后->',body);
 
 $done(body);
